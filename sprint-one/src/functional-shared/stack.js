bc8311d775727fd7b3bbea5f78151350f8812e25
James Jelenko <jamesjelenko@gmail.com>
2017-11-29 20:45:09 -0800

    None of these are working completely. We tried a variety of things -- resuming work in the morning. Proto stack is close -- constructor prototype issue.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,30 +1,27 @@
-var Stack = function() {
-  var instanceObject = {};       
-  var storage = {1: 'A'};
-  return instanceObject;
-};
-
-var methods = {
-
-  push: function(value) { 
-    //console.log('hello');
-    console.log(this.storage);
-  },
-  'pop': function() {
-  },
-  'size': function() {
-  }
-};
-
-var s = Stack(methods);
-
 var extend = function(obj1, obj2) {
-  var keys = Object.keys(obj2);
-  keys.forEach(key => obj1[key] = obj2[key]);
+  for (var key in obj2){
+    obj1[key] = obj2[key];
+  }
   return obj1;
 };
 
+var Stack = function() {
+  var instanceObject = {};       
+  var storage = [];
+  extend(instanceObject, methods)
+  return instanceObject;
+};
+
+var methods = {};
+methods.push = function(value) { 
+    this.storage.push(value);
+  }
+methods.pop = function() {
+    var target = this.storage.pop();
+    return target
+  }
+methods.size = function() {
+    return this.storage.length;
+  }
+
         
-
-
-
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,23 +1,19 @@
 var Stack = function() {
   var someInstance = Object.create(Stack.prototype);
   someInstance.storage = [];
+
   return someInstance;
 };
 
-Stack.prototype.push = function (value) {
-  this.storage.push(value);
-};
 
-Stack.prototype.pop =  function () {
-  var removedValue = this.storage.pop();
-  return removedValue;
-};
-
-// 	}, 
-
-// 	size: function() {
-
-// 	}
-// };
-
-
+  
+  Stack.prototype.push = function (value) {
+  	this.storage.push(value);
+	};
+	Stack.prototype.pop =  function () {
+  	var removedValue = this.storage.pop();
+  	return removedValue;
+	};
+	Stack.prototype.size = function(){
+		return this.storage.length;
+	}
