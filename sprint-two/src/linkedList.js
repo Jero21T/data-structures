d2fc2d3e64b6d736f7e8ea054c81366cd613f44b
James Jelenko <jamesjelenko@gmail.com>
2017-11-30 23:28:37 -0800

    Cleaned up some unnecessary / redundant code
    Polished up my ugly, slapdash solution from earlier tonight. Still needs to be tested against specs, I've only written a few basic operations and console logs here.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -48,3 +48,63 @@ var Node = function(value){
   node.next = null;
   return node
 }
+
+/*
+// Refactored to remove redundancy and unnecessary code.
+// Needs to be tested in suite.
+
+var LinkedList = function(){
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value){
+    if(!this.head){
+      this.head = Node(value);
+      this.tail = this.head;
+    } else {
+      this.tail.next = Node(value);
+      this.tail = this.tail.next;
+    }
+  };
+
+  list.removeHead = function(){
+    var oldHead = this.head;
+    this.head = this.head.next;
+    return oldHead.value;
+  };
+
+  list.contains = function(target){
+    var node = this.head;
+    if (!node){
+      return false;
+    }
+    while (node.next) {
+      if (node.value === target) {
+        return true;
+      } 
+      node = node.next;
+    }
+    return false;
+  };
+  return list;
+};
+
+var Node = function(value){
+  var node = {};
+  node.value = value;
+  node.next = null;
+  return node
+}
+
+var l = LinkedList();
+l.addToTail("1");
+l.addToTail("2");
+l.addToTail("3");
+l.addToTail("4");
+console.log(l);
+console.log(`Contains 3? ${l.contains("3")}`);
+l.removeHead();
+console.log(l);
+
+*/
