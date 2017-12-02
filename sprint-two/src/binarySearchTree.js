99353ba6583e8f21733ffe3b99f8ebbbf6a850bf
Hack Reactor Student <student@hackreactor.com>
2017-12-02 10:17:34 -0800

    #contains now returns boolean and parses tree. Still not returning correct value  / not passing specs.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,53 @@
 var BinarySearchTree = function(value) {
+  var someInstance = Object.create(BinarySearchTree.prototype);
+  someInstance.value = value;
+  someInstance.left = [];
+  someInstance.right = [];
+  return someInstance;  
+};
+
+BinarySearchTree.prototype.insert = function(node) {
+  
+};
+
+BinarySearchTree.prototype.contains = function(target) {
+  var result; 
+  
+  var topNode = this;
+
+  var checkNode = function(node) {
+    if (node.value === target) {
+      result = true;
+    };
+
+    if (node.value > target) {
+    if (node.left.length) {
+      node.left.forEach(function(child) {
+        checkNode(child)
+      });
+    } else {
+        result = false;
+    }
+  } else {
+      if (node.right.length) {
+        node.right.forEach(function(child) {
+          checkNode(child);
+        });
+      } else {
+          result = false;
+        }
+    }
+ };
+
+ checkNode(topNode);
+ return result ? true : false;
 };
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
+
+
+
+BinarySearchTree.prototype.depthfirstLog = function (callback) {
+
+};
