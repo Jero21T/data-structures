3cdc3303f1a3608e72f1beb8c780740c11e2ea76
Hack Reactor Student <student@hackreactor.com>
2017-12-02 09:17:54 -0800

    'Finished the correct implementation of HashTable'

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,37 +1,3 @@
-var HashTable = function() {
-  this._limit = 8;
-  this._storage = LimitedArray(this._limit);
-};
-
-HashTable.prototype.insert = function(k, v) {
-
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  
-  if (!this._storage[index]) {
-    this._storage[index] = [k, v];
-    this._storage[index] =  this.getTuple(k, v);
-  }
-};
-
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage[index]) {
-    if (this._storage[index][k]) {
-      return this._storage[index][k];
-    }
-  }
-  return this._storage[index];
-};
-
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, null);
-};
-
-HashTable.prototype.getTuple = function(k, v) {
-  return [k, v];
-};
-
 
 
 
@@ -39,3 +5,41 @@ HashTable.prototype.getTuple = function(k, v) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+var HashTable = function() {
+  this._limit = 8;
+  this._storage = LimitedArray(this._limit);
+};
+HashTable.prototype.insert = function(k, v) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k, v];
+  if (!this._storage[index]) {
+    this._storage[index] = [];
+    this._storage[index].push(tuple);
+  }
+  this._storage[index].push(tuple);
+};
+HashTable.prototype.retrieve = function(k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucketToSearchIn = this._storage[index];
+  var valueToBeRetrieved;
+  bucketToSearchIn.forEach(function(tuple) {
+    if (tuple[0] === k) {
+      valueToBeRetrieved = tuple[1];
+    }
+  });
+  
+  return valueToBeRetrieved;
+};
+HashTable.prototype.remove = function(k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucketToSearchIn = this._storage[index];
+  var valueToBeRemoved;
+  var indexOfValueToBeRemoved;
+  var remainingElements = [];
+  bucketToSearchIn.forEach(function(tuple, index) {
+    if (tuple[0] !== k) {
+      remainingElements.push(tuple);
+    }
+  });
+  this._storage[index] = remainingElements;
+};
