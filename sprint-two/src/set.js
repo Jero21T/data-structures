14426cbfefa271400930e857517cb8c2540d58e3
Hack Reactor Student <student@hackreactor.com>
2017-12-01 16:13:23 -0800

    Set passing all tests.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+var targetIndex;
+  this._storage.forEach(function(element, index) {
+    if (item === element){
+      targetIndex = index;
+    }
+  });
+  this._storage.splice(targetIndex, 1);
 };
 
 /*
