78acbc0d4e5cbb8ee3c287ae8bbcd5ba743b86c0
Hack Reactor Student <student@hackreactor.com>
2017-12-02 14:26:26 -0800

    Passing all tests except depthFirstLog.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,54 +1,77 @@
 var BinarySearchTree = function(value) {
   var someInstance = Object.create(BinarySearchTree.prototype);
   someInstance.value = value;
-  someInstance.left = [];
-  someInstance.right = [];
+  someInstance.left = null;
+  someInstance.right = null;
   return someInstance;  
 };
 
-BinarySearchTree.prototype.insert = function(node) {
+
+BinarySearchTree.prototype.insert = function(value) {
+  var newNode = BinarySearchTree(value);
+  var current = this;
+  
+  if (newNode.value < current.value) {
+    if (!current.left) {
+      current.left = newNode;
+    } else {
+      current.left.insert(value);
+    }
+  }
+  if (newNode.value > current.value) {
+    if (!current.right) {
+      current.right = newNode;
+    } else {
+      current.right.insert(value);
+    }
+  }
   
 };
 
 BinarySearchTree.prototype.contains = function(target) {
-  var result; 
-  
+  var result;   
   var topNode = this;
-
   var checkNode = function(node) {  
+  
     if (node.value === target) {
-      result = true;
-      return
-    };
+      return true;
+    }
+    if (target < node.value) {
+      if (node.left) {
+        return checkNode(node.left);
+      } 
+    } 
+    if (target > node.value) {
+      if (node.right) {
+        return checkNode(node.right);
+      } 
+    }
+    return false;
+  };
 
-    if (node.value > target) {
-    if (node.left.length) {
-      node.left.forEach(function(child) {
-        checkNode(child)
-      });
-    } else {
-        result = false;
-    }
-  } else {
-      if (node.right.length) {
-        node.right.forEach(function(child) {
-          checkNode(child);
-        });
-      } else {
-          result = false;
-        }
-    }
- };
- 
- checkNode(topNode);
- return result ? true : false;
+  return checkNode(topNode);
 };
 
 
 
 
-
-
-BinarySearchTree.prototype.depthfirstLog = function (callback) {
-
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+  var vals = [];
+  var node = this;
+  vals.push(this.value);
+  var getVals = function() {
+    if (this.left) {
+      this.left.getVals();
+    } else {
+      vals.push(this.val);
+    }
+    if (this.right) {
+      this.right.getVals();
+    } else {
+      vals.push(this.val);
+    }
+    
+  };
+  getVals(node);
+  console.log(vals);
 };
